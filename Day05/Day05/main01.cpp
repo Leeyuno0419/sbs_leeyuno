43c895dc86a333fbb402314a692f3d7640439322
Leeyuno0419 <154118375+Leeyuno0419@users.noreply.github.com>
2024-02-21 21:49:34 +0900

    +Day05

---
+++ Day05/Day05/main01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	/*
+	* day05 [반복문, 배열, 함수]
+	*/
+}
---
+++ Day05/Day05/main03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	int i = 0;
+	while(1)
+	{
+		printf("%d", i++);
+	}
+}
